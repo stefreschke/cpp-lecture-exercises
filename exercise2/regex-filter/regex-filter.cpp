05145ce267de2b0730a7d719b06718a098e8d6b9
Stefan Reschke <stefan.reschke@outlook.de>
2021-05-20 07:46:33 +0200

    regex finished

--- exercise2/regex-filter/regex-filter.cpp
+++ exercise2/regex-filter/regex-filter.cpp
@@ -3,14 +3,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
 
 using namespace std::string_literals;
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
     if (argc < 2) {
         std::cerr << "No regex passed" << std::endl;
-
         return -1;
     }
 
@@ -46,15 +46,42 @@ int main(int argc, char* argv[])
 
         lines.push_back(std::move(line));
     }
-    
-    // Todo: filter
-    
-    // Todo: count
-    
-    // Todo: output
 
-    for (const auto & line : lines) {
-        std::cout << line << '\n';
+    std::string str(argv[1]);
+    bool case_sensitive = true;
+    if (argc > 2) {
+        std::string sensitivity_arg(argv[2]);
+        if (sensitivity_arg == "icase") {
+            case_sensitive = false;
+        }
+    }
+    std::regex re;
+    if (case_sensitive) {
+        re = std::regex(str);
+    } else {
+        re = std::regex(str, std::regex_constants::icase);
+    }
+    std::map<std::string, int> map{};
+    for (const auto & l : lines) {
+        std::smatch base_match;
+        if (std::regex_match(l, base_match, re)) {
+            if (base_match.size() >= 2) {
+                auto sub_match_str = base_match[1].str();
+                if (!case_sensitive) {
+                    std::transform(sub_match_str.begin(), sub_match_str.end(), sub_match_str.begin(),
+                                    [](unsigned char c){ return std::tolower(c); });
+                }
+                if (map.contains(sub_match_str)) {
+                    auto stuff = map[sub_match_str];
+                    map[sub_match_str] = stuff + 1;
+                } else {
+                    map[sub_match_str] = 1;
+                }
+            }
+        }
+    }
+    for (const auto& entry : map) {
+        std::cout << entry.first << ": " << entry.second << std::endl;
     }
 
     std::cout.flush();
