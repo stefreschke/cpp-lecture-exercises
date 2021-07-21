eb9948e6176015c72fe047af43cfddfd58302996
Stefan Reschke <stefan.reschke@outlook.de>
2021-07-21 20:28:15 +0200

    having this task working, sort of

--- exercise6/native-module-python/build_n_run.sh
+++ exercise6/native-module-python/build_n_run.sh
@@ -1,4 +1,8 @@
+# have python3 available on path (no other requirements)
 python3 -m venv venv
 source venv/bin/activate
 pip install cffi
+echo "=============="
+echo "running program"
+echo "=============="
 python3 main.py
--- exercise6/native-module-python/main.py
+++ exercise6/native-module-python/main.py
@@ -1,11 +1,14 @@
 
-# Import process function from pre-built cffi module
-process = Null
-
 from cffi import FFI
+ffibuilder = FFI()
+ffibuilder.cdef("int process(int n);")
+ffibuilder.set_source("_process",
+                      '#include "process.h"',
+                      sources=['process.c'],
+                      libraries=["m"])
 
-
-if __name__=="__main__":
-	for i in range(1, 101):
-		print("Call with", i, "results in", process(i))
-
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=False)
+    from _process.lib import process
+    for i in range(1, 101):
+        print("Call with", i, "results in", process(i))
