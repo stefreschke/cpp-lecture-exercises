4bdae5afe4ca19d51b57686936d4207cacdacbff
Stefan Reschke <stefan.reschke@outlook.de>
2021-07-21 19:59:41 +0200

    towards solving this stupid task

--- exercise6/native-module-python/main.py
+++ exercise6/native-module-python/main.py
@@ -2,6 +2,9 @@
 # Import process function from pre-built cffi module
 process = Null
 
+from cffi import FFI
+
+
 if __name__=="__main__":
 	for i in range(1, 101):
 		print("Call with", i, "results in", process(i))
